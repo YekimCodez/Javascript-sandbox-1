4bf8317da100c5044c4d650d3996da74c13a4577
Michael Doughty <yekim.codes@gmail.com>
2026-06-24 11:20:00 -0400

    I created a json file and added three users with some information. I wrote a function called getJsonData() and used fetch to get the users.json. I created a h2 tag and gave it an id of output and a class of output. I referenced the h2 in a variable called output and gave it some content. I used a forEach loop to loop through the json data using the += operator on the output variable and gave it some content a ul tag and three li tags each with user.id user.name and user.email. I referenced the h2 element with the id output and made it's innerHTML = output.'

--- Api's/api.js
+++ Api's/api.js
@@ -1,4 +1,39 @@
 const button = document.getElementById('fetchData');
+const btn = document.createElement('button');
+btn.textContent = 'JSON Data';
+btn.id = 'jsonBtn';
+btn.classList.add('json-btn');
+document.body.appendChild(btn);
+
+btn.addEventListener('click', getJsonData);  
+
+let h2 = document.createElement('h2');
+document.body.appendChild(h2);
+h2.id = 'output';
+h2.classList.add('output');
+
+function getJsonData() {
+    fetch('users.json')
+    .then((res) => {
+        return res.json()
+    })
+
+    .then((jsonData) => {
+        let output = '<h2>Users</h2>';
+        jsonData.forEach(function(user) {
+        output += `
+                <ul>
+                    <li>${user.id}</li>
+                    <li>${user.name}</li>
+                    <li>${user.email}</li>            
+                </ul>           
+            `;
+        })
+        document.getElementById('output').innerHTML = output;
+        console.log(jsonData);
+    })
+}
+
 
 button.addEventListener('click', getData);
 
@@ -21,9 +56,9 @@ function getData() {
         document.body.appendChild(dataContainer);
 
         console.log('Data container added: ', dataContainer);
-        console.log('Data element added: ', dataElement);
-        
-    });
+        console.log('Data element added: ', dataElement);        
+    })
+    .catch((error) => console.error('error fetching data', error));
 }
 
 style.textContent = `
